953b65ffdf07443e4a03e94a7f57e45f6399f5fb
hemantttt727 <hemantsoni8005@gmail.com>
2025-06-27 14:13:14 +0530

    Create Index.js

---
+++ Page/Index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Chat Fix App!</h1>
+      <p>Explore login, map, memes, profile, game.</p>
+    </div>
+  );
+}
